7175ffc205ec0217a814385403f7b4a11f0e8818
MaxCintra <553mec@gmail.com>
2023-09-13 22:27:30 -0500

    C

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ import numpy as np
 app = Flask(__name__)
 
 # Load your trained machine learning model
-model1 = joblib.load('model3_v2.joblib')  # Replace 'your_model_file.pkl' with the actual filename
-model2 = joblib.load('model3_v2.joblib')  # Replace 'your_model_file.pkl' with the actual filename
-model3 = joblib.load('model3_v2.joblib')  # Replace 'your_model_file.pkl' with the actual filename
+model1 = joblib.load('probowl_prediction.joblib')  
+model2 = joblib.load('allpro_prediction.joblib')
 
 # Define a route for the home page
 @app.route('/')
@@ -17,7 +16,7 @@ def home():
     return render_template('index.html')
 
 # Define a route to handle user input and make predictions
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         # Get user input from the form
@@ -30,18 +29,21 @@ def predict():
         feature6 = float(request.form['feature6'])
         feature7 = float(request.form['feature7'])
         feature8 = float(request.form['feature8'])
+        feature9 = float(request.form['feature9'])
+        feature10 = float(request.form['feature10'])
+        feature11 = float(request.form['feature11'])
+        feature12 = float(request.form['feature12'])
         # Repeat this for all the features needed for your prediction
 
         # Make predictions using the loaded machine learning model
-        input_data = np.array([[feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8]])  # Create an input array
+        input_data = np.array([[feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9, feature10, feature11, feature12]])  # Create an input array
         prediction1 = model1.predict(input_data)
         prediction2 = model2.predict(input_data)
-        prediction3 = model3.predict(input_data)
-
         # You can format the prediction or use it as needed for display
         # For example, you can convert it to a string and round it to a specific number of decimal places
-        prediction_str = f"Predicted Result: {round(prediction1[0], 2)}"
-        return render_template('result.html', prediction=prediction_str)
-
+        prediction_str1 = f"Predicted Result: {round(prediction1[0], 2)}"
+        prediction_str2 = f"Predicted Result: {round(prediction2[0], 2)}"
+        return render_template('result.html', prediction1=prediction_str1, prediction2=prediction_str2)
+    
 if __name__ == '__main__':
     app.run(debug=True)
